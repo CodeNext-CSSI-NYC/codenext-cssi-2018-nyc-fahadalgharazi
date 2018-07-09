c5f61a8bc7ecf0a1048b5fd263abee66d251c02a
fahad <fahad.algharazi@csedge.org>
2018-07-09 16:36:39 -0400

    math practice done

--- hello.js
+++ hello.js
@@ -1,3 +1,3 @@
 // Author: fahad algharazi
 console.log("hello world");
-console.log("2+2 is 4 minus 1 is 3 quick mafs")
+console.log("2+2 is 4 minus 1 is 3 quick mafs11")
--- math-practice.js
+++ math-practice.js
@@ -1,6 +1,8 @@
+// author= fahad algharazi
+
 // Under each question, write the code that solves it, and then write your answer in a comment
 // For example, if the question is: What's the sum of 79 and 2018 you would write:
-console.log(79 + 2018);
+// console.log(79 + 2018);
 // 2097
 
 // Some of the questions will use these variables
@@ -9,15 +11,20 @@ const q = 60;
 const r = 25;
 
 // 1. Is 91 divisible by 13? (The answer should be either true or false)
-
+//console.log(91/13);
+			// true
 // 2. What is the result when the q divided by r is rounded to the nearest integer?
-
+	//console.log(60/25);
+				// 2.4
 // 3. Is the product of p and r greater than q squared? (The answer should be either true or false)
-
+//	console.log((27 * 25) - 60**2)
+			// false
 // 4. What is the remainder when the sum of q and r is divided by p?
-
+	// console.log((60+25)%12);
+				// 1
 // 5. 27 squared is equal to what number raised to the third power?
-
+		// console.log(Math.cbrt(729));
+		// 9
 // 6. What is the absolute value of p - q? (Find out how to do absolute value by searching online.)
-
+ 	console.log(Math.abs(12-60));
 // 7. Find another math operation we haven't talked about yet on the MDN (Mozilla Developer Network) and try it out below.
