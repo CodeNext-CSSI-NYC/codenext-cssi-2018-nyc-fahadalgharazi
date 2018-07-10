2a86c39251494c31e742000e21fd1dca420cb659
fahad <fahad.algharazi@csedge.org>
2018-07-10 12:27:57 -0400

    finsihed

--- hello.js
+++ hello.js
@@ -2,4 +2,4 @@
 var readline = require("readline-sync");
 var userName = readline.question("please enter your name or else!!")
 console.log("hello " + userName);
-console.log("2+2 is 4 minus 1 is 3 quick mafs11")
+console.log("2+2 is 4 minus 1 is 3 quick mafs11");
