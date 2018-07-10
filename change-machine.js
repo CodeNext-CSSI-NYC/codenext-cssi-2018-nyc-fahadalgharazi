103509a5bacf6cd79642eb8df77c3e711d9008fc
fahad <fahad.algharazi@csedge.org>
2018-07-10 12:57:08 -0400

    finished making the equation

--- change-machine.js
+++ change-machine.js
@@ -1,1 +1,27 @@
-// Author: FirstName LastName
+// Author: Fahad algharazi
+var readline = require("readline-sync");
+console.log("welcome to the CHANGE MACHINE");
+ var cents = readline.question(Enter the cents as a positive integer);
+// var q = 25;
+ var quarter;  
+ var dime;
+ var nickel;
+ var penny;
+// var d = 10;
+// var dime = 
+// var n = 5;
+// var nickel = 
+// var p = 1;
+// var penny = 1
+// if(y % 25 = 0){
+//  y/q=
+// }
+quarter = math.floor(cents/25);
+console.log("quarter = " + quarter);
+ remeinder = cents%25;
+ dime =remeinder/10;
+ remeinder=remeinder%10;
+ nickel=remeinder/5;
+ remeinder=remeinder%5;
+ penny=remeinder/1;
+
