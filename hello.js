ecc922723e221c2551b53583f4301e3ddc1f563b
fahad <fahad.algharazi@csedge.org>
2018-07-10 10:38:09 -0400

    frst time using readline

--- hello.js
+++ hello.js
@@ -1,3 +1,5 @@
 // Author: fahad algharazi
-console.log("hello world");
+var readline = require("readline-sync");
+var userName = readline.question("please enter your name or else!!")
+console.log("hello " + userName);
 console.log("2+2 is 4 minus 1 is 3 quick mafs11")
