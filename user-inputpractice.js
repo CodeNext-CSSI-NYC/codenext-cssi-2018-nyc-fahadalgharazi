62bf3e9d7da195c3d01b17376beb724cc9e04636
fahad <fahad.algharazi@csedge.org>
2018-07-10 11:33:32 -0400

    averaging numbs

--- user-inputpractice.js
+++ user-inputpractice.js
@@ -3,9 +3,29 @@
 // Try this as you move from question to question below.
 var readline = require("readline-sync");
 // 1. Ask the user to enter a number, and print out "The square root of ___ is ___ ", but with the blanks filled in correctly. (Not sure how to get the square root of a number in JavaScript? Google it!)
-
+	// var numberQuestion = readline.question("Enter any number!!");
+	// console.log("The square root of " + numberQuestion);
+	// console.log("isssss drumroll please " + Math.sqrt(numberQuestion));
 // 2. Ask the user to enter their name, and print out "Your name has ___ letters", but with the blank filled in. Not sure how to get the number of letters in a string in JavaScript? Google it!
-
+   // var name = readline.question("enter a name?")
+   
+   // console.log("The name you wrote is too basic it has only " + name.length + " letters  GIT GUD!!");
 // 3. Ask the user to enter any word, and print out "The word ____ starts with the letter ___", but with the blanks filled in. Not sure how to get the first letter of a string in JavaScript? ...
-
+// var first = readline.question("Enter a word any word you can think of ...");
+// console.log("The word " + first +" starts with the letter " + first.substring(0,1));
 // 4. Ask the user to enter two numbers, and print out "The average of ___ and ___ is ____", but with the blanks filled in.
+var number = readline.question("Enter a number...");
+var number2 = readline.question("Enter another number...");
+console.log("The average of " + number +" and" + number2 + " is" + (number + number2)/2);
+
+
+
+
+
+
+
+
+
+
+
+
