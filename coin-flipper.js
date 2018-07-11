f32c8a551b1af21afcc2f5c7c6f09c2e328f0519
fahad <fahad.algharazi@csedge.org>
2018-07-11 15:57:43 -0400

    finished coin flipper game

---
+++ coin-flipper.js
@@ -0,0 +1,57 @@
+//author:fahad algharazi
+
+var readline = require("readline-sync");
+
+
+var guess = readline.question("guess heads or tails=-=-=->");
+
+
+
+ var ran = Math.random()
+if(0.5<ran&& guess =="heads"){
+	console.log("you guessed correctly");
+}
+else if(0.5<ran && guess == "tails"){
+	console.log("you guessed wrong better luck next time");
+}
+else if(0.5>ran && guess == "heads"){
+	console.log("you guessed wrong better luck next time");
+}
+else if(0.5>ran && guess == "tails"){
+	console.log("you guessed correctly");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
