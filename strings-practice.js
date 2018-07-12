b40288348c7154b9ecfb90c03ae50c76b3bad5b9
fahad <fahad.algharazi@csedge.org>
2018-07-12 16:48:34 -0400

    finished both

--- strings-practice.js
+++ strings-practice.js
@@ -7,7 +7,7 @@ let readline = require("readline-sync");
 // makeAbba("Hi", "Bye") → "HiByeByeHi"
 // makeAbba("Yo", "Alice") → "YoAliceAliceYo"
 // makeAbba("What", "Up") → "WhatUpUpWhat" 
-
+/*
 function makeAbba(a, b) {
 	return (a + b + b +a);
 
@@ -16,11 +16,29 @@ let a = readline.question("write a word:");
 let b = readline.question("write a word:");
 let result = makeAbba(a,b);
 console.log(result);
+*/
 
+//Given 2 strings, a and b, return a string of the form short+long+short, with the shorter string on the outside and the longer string on the inside. The strings will not be the same length, but they may be empty (length 0).
+// Examples: 
+// comboString("Hello", "hi") → "hiHellohi"
+// comboString("hi", "Hello") → "hiHellohi"
+// comboString("aaa", "b") → "baaab" 
 
+function comboString(a, b) {
+ aLength = (a.length);
+ bLength = (b.length);
+ 
+	if(aLength>bLength){
+		return (b + a + b);
+	}else{
+		return (a+b+a);
+	}
+}
 
-
-
+let a = readline.question("write a word:");
+let b = readline.question("write a word:");
+let result = comboString(a,b);
+console.log(result);
 
 
 
