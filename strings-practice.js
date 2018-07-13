9bf8070a796ea0058cc51a3106d639c99c7b4043
fahad <fahad.algharazi@csedge.org>
2018-07-13 11:58:39 -0400

    finished wloops prac

--- strings-practice.js
+++ strings-practice.js
@@ -20,21 +20,20 @@ console.log(result);
 
 //Given 2 strings, a and b, return a string of the form short+long+short, with the shorter string on the outside and the longer string on the inside. The strings will not be the same length, but they may be empty (length 0).
 // Examples: 
-// comboString("Hello", "hi") → "hiHellohi"
-// comboString("hi", "Hello") → "hiHellohi"
-// comboString("aaa", "b") → "baaab" 
+comboString("Hello", "hi") → "hiHellohi"
+comboString("hi", "Hello") → "hiHellohi"
+comboString("aaa", "b") → "baaab" 
 
 function comboString(a, b) {
- aLength = (a.length);
- bLength = (b.length);
- 
+	aLength = (a.length);
+	bLength = (b.length);
+
 	if(aLength>bLength){
 		return (b + a + b);
 	}else{
 		return (a+b+a);
 	}
 }
-
 let a = readline.question("write a word:");
 let b = readline.question("write a word:");
 let result = comboString(a,b);
